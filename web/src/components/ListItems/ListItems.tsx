1f9e8edcc4ff915346845932f49a3ea6ebb48fec
Jake Hash <1508098+jhash@users.noreply.github.com>
2023-08-25 15:37:10 -0400

    Committing reservations styling

--- web/src/components/ListItems/ListItems.tsx
+++ web/src/components/ListItems/ListItems.tsx
@@ -56,6 +56,76 @@ const FormSaveButton = ({ ...props }) => (
   </button>
 )
 
+const useListItemReservations = ({
+  reservations,
+  listItem,
+}: {
+  reservations: ListItemsQuery['listItems'][number]['reservations']
+  listItem: ListItemsQuery['listItems'][number]
+}) => {
+  const { currentUser } = useAuth()
+
+  const nonReleasedReservations = useMemo(
+    () =>
+      (reservations || []).filter(
+        (reservation) => reservation.status !== 'RELEASED'
+      ),
+    [reservations]
+  )
+
+  const userReservations = useMemo(
+    () =>
+      currentUser
+        ? reservations.filter(
+            (reservation) => reservation.userId === currentUser?.id
+          )
+        : [],
+    [currentUser, reservations]
+  )
+
+  const reserved = !!nonReleasedReservations.length
+  const nonReleasedUserReservations = useMemo(
+    () =>
+      userReservations.filter(
+        (reservation) => reservation.status !== 'RELEASED'
+      ),
+    [userReservations]
+  )
+  const quantityReservedByUser = useMemo(
+    () => nonReleasedUserReservations.length,
+    [nonReleasedUserReservations]
+  )
+  const reservedByUser = useMemo(
+    () => !!quantityReservedByUser,
+    [quantityReservedByUser]
+  )
+  const reservedByOthers = !userReservations?.length && !!reserved
+  const quantityReserved = useMemo(
+    () =>
+      nonReleasedReservations.reduce(
+        (total, reservation) => total + (reservation.quantity || 1),
+        0
+      ),
+    [nonReleasedReservations]
+  )
+  const quantityReservedByOthers = quantityReserved - quantityReservedByUser
+  const allReserved = quantityReserved >= listItem.quantity
+  const remaining = Math.max(listItem.quantity - quantityReserved, 0)
+
+  return {
+    remaining,
+    nonReleasedReservations,
+    userReservations,
+    reservedByUser,
+    reservedByOthers,
+    quantityReserved,
+    allReserved,
+    quantityReservedByUser,
+    nonReleasedUserReservations,
+    quantityReservedByOthers,
+  }
+}
+
 const RESERVATION_STATUS_OPTIONS: {
   value: ReservationStatus
   name: string
@@ -89,50 +159,18 @@ const ListItemReservationButtons: React.FC<ListItemReservationsButtons> = ({
     Partial<Reservation> | undefined
   >()
 
-  const { currentUser } = useAuth()
-
   const reservationsAllowed = useMemo(
     () => reservationsEnabled(list?.type),
     [list?.type]
   )
 
-  const nonReleasedReservations = useMemo(
-    () =>
-      (reservations || []).filter(
-        (reservation) => reservation.status !== 'RELEASED'
-      ),
-    [reservations]
-  )
-
-  const userReservations = useMemo(
-    () =>
-      currentUser
-        ? reservations.filter(
-            (reservation) => reservation.userId === currentUser?.id
-          )
-        : [],
-    [currentUser, reservations]
-  )
-
-  const reserved = !!nonReleasedReservations.length
-  const reservedByUser = useMemo(
-    () =>
-      !!userReservations.length &&
-      !!userReservations.filter(
-        (reservation) => reservation.status !== 'RELEASED'
-      ).length,
-    [userReservations]
-  )
-  const reservedByOthers = !userReservations?.length && !!reserved
-  const quantityReserved = useMemo(
-    () =>
-      nonReleasedReservations.reduce(
-        (total, reservation) => total + (reservation.quantity || 1),
-        0
-      ),
-    [nonReleasedReservations]
-  )
-  const allReserved = quantityReserved >= listItem.quantity
+  const {
+    userReservations,
+    reservedByUser,
+    reservedByOthers,
+    quantityReserved,
+    allReserved,
+  } = useListItemReservations({ reservations, listItem })
 
   const [createReservationMutation, { loading: createLoading }] = useMutation(
     CREATE_RESERVATION_MUTATION,
@@ -198,6 +236,8 @@ const ListItemReservationButtons: React.FC<ListItemReservationsButtons> = ({
 
     const status: ReservationStatus = field.value
 
+    const { currentUser } = useAuth()
+
     return (
       <>
         {!reservation.id && (
@@ -209,26 +249,6 @@ const ListItemReservationButtons: React.FC<ListItemReservationsButtons> = ({
             />
             <NumberField hidden name="userId" defaultValue={currentUser?.id} />
           </>
-        )}
-        {!!reservedByOthers && (
-          <p
-            className={classNames(
-              'text-lg font-bold leading-normal',
-              allReserved ? 'text-warning' : 'text-info'
-            )}
-          >{`${allReserved ? 'WARNING: it looks like ' : ''}${
-            allReserved && listItem.quantity > 1
-              ? 'all'
-              : `${quantityReserved}${
-                  listItem.quantity > 1 ? ` out of ${listItem.quantity}` : ''
-                }`
-          } of these items ${
-            quantityReserved === 1 ? 'has' : 'have'
-          } been reserved.${
-            allReserved
-              ? ' You might want to hold off on reserving this item as well.'
-              : ''
-          }`}</p>
         )}
         <FormItem
           type="select"
@@ -315,6 +335,26 @@ const ListItemReservationButtons: React.FC<ListItemReservationsButtons> = ({
           open={!!reservation}
           id="reserveItem"
         >
+          {!!reservedByOthers && (
+            <p
+              className={classNames(
+                'text-lg font-bold leading-normal',
+                allReserved ? 'text-warning' : 'text-info'
+              )}
+            >{`${allReserved ? 'WARNING: it looks like ' : ''}${
+              allReserved && listItem.quantity > 1
+                ? 'all'
+                : `${quantityReserved}${
+                    listItem.quantity > 1 ? ` out of ${listItem.quantity}` : ''
+                  }`
+            } of these items ${
+              quantityReserved === 1 ? 'has' : 'have'
+            } been reserved.${
+              allReserved
+                ? ' You might want to hold off on reserving this item as well.'
+                : ''
+            }`}</p>
+          )}
           {!!listItem.title && (
             <div className="flex flex-col gap-2">
               <div className="text-lg font-bold">{'Item'}</div>
@@ -366,57 +406,106 @@ const ListItemReservationButtons: React.FC<ListItemReservationsButtons> = ({
   )
 }
 
-const ListItems: React.FC<ListItemsQuery> = ({ listItems }) => {
-  return listItems.map((listItem, index) => {
-    const { id, url, title, description, quantity, list } = listItem
+const ListItem: React.FC<{ listItem: ListItemsQuery['listItems'][number] }> = ({
+  listItem,
+}) => {
+  const { id, url, title, description, quantity, list, reservations } = listItem
 
-    const reservations = reservationsEnabled(list.type)
+  const reservationsAllowed = reservationsEnabled(list.type)
 
-    const QuantityText = () => {
-      if (!quantity || !reservations) {
-        return null
-      }
+  const { allReserved, quantityReservedByUser, quantityReserved, remaining } =
+    useListItemReservations({ reservations, listItem })
 
-      return (
-        <div className="flex items-center gap-[0.15rem] text-sm font-medium text-gray-400">
-          <X size="0.75rem" />
-          {quantity}
-        </div>
-      )
+  const QuantityText = () => {
+    if (!quantity || !reservationsAllowed) {
+      return null
     }
 
     return (
-      <li
-        key={id || index}
-        className="flex cursor-pointer items-center gap-3 rounded-lg border pr-3 leading-tight shadow-sm hover:bg-gray-300 hover:bg-opacity-30 dark:hover:bg-gray-600 dark:hover:bg-opacity-20"
-      >
-        {url ? (
-          <ExternalLink
-            href={url}
-            className="link min-h-12 flex flex-grow items-center gap-3 px-3 py-1 text-lg no-underline"
+      <>
+        {quantity > 1 && (
+          <div
+            className={classNames(
+              'flex items-center gap-[0.15rem] text-sm font-medium',
+              'text-gray-400'
+            )}
           >
-            {title}
-            <Link2 size="1rem" className="flex-shrink-0" />
-            <QuantityText />
-          </ExternalLink>
-        ) : (
-          <div className="link min-h-12 flex flex-grow items-center gap-3 px-3 py-1 text-lg no-underline">
-            {title}
-            <QuantityText />
+            <X size="0.75rem" />
+            {quantity}
           </div>
         )}
-
-        <div className="flex items-center justify-end gap-3">
-          {!!description && (
-            <span className="whitespace-normal text-right text-sm text-gray-500 dark:text-gray-400">
-              {description}
-            </span>
-          )}
-          <ListItemReservationButtons listItem={listItem} />
-        </div>
-      </li>
+        {allReserved ? (
+          <div
+            className={classNames(
+              'flex items-center text-sm font-medium uppercase text-success'
+            )}
+          >
+            {`reserved`}
+          </div>
+        ) : (
+          <>
+            {!!quantityReservedByUser && (
+              <div
+                className={classNames(
+                  'flex items-center text-sm font-medium text-info'
+                )}
+              >
+                {`${quantityReservedByUser} reserved`}
+              </div>
+            )}
+            {!!quantityReserved && !!remaining && (
+              <div
+                className={classNames(
+                  'flex items-center text-sm font-medium',
+                  'text-secondary'
+                )}
+              >
+                {`${remaining} to go`}
+              </div>
+            )}
+          </>
+        )}
+      </>
     )
-  })
+  }
+
+  return (
+    <li
+      key={id}
+      className="flex cursor-pointer items-center gap-3 rounded-lg border pr-3 leading-tight shadow-sm hover:bg-gray-300 hover:bg-opacity-30 dark:hover:bg-gray-600 dark:hover:bg-opacity-20"
+    >
+      {url ? (
+        <ExternalLink
+          href={url}
+          className="link min-h-12 flex flex-grow items-center gap-3 px-3 py-1 text-lg no-underline"
+        >
+          {title}
+          <Link2 size="1rem" className="flex-shrink-0" />
+          <QuantityText />
+        </ExternalLink>
+      ) : (
+        <div className="link min-h-12 flex flex-grow items-center gap-3 px-3 py-1 text-lg no-underline">
+          {title}
+          <QuantityText />
+        </div>
+      )}
+
+      <div className="flex items-center justify-end gap-3">
+        {!!description && (
+          <span className="whitespace-normal text-right text-sm text-gray-500 dark:text-gray-400">
+            {description}
+          </span>
+        )}
+        <ListItemReservationButtons listItem={listItem} />
+      </div>
+    </li>
+  )
+}
+
+const ListItems: React.FC<ListItemsQuery> = ({ listItems }) => {
+  return listItems.map((listItem, index) => (
+    <ListItem key={index} listItem={listItem} />
+  ))
 }
 
 export default ListItems
