6298c75df2d33365d0215642c49d03a1c48db1cc
Jake Hash <1508098+jhash@users.noreply.github.com>
2023-08-25 21:56:29 -0400

    Following guides to get puppeteer working on vercel

--- api/src/functions/digestLink/amazon.ts
+++ api/src/functions/digestLink/amazon.ts
@@ -1,9 +1,26 @@
-import chromium from 'chrome-aws-lambda'
-import puppeteer from 'puppeteer'
+let chrome = {
+  args: [],
+  defaultViewport: '',
+  executablePath: undefined,
+}
+let puppeteer
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require('chrome-aws-lambda')
+  puppeteer = require('puppeteer-core')
+} else {
+  // running locally.
+  puppeteer = require('puppeteer')
+}
+
+const LOCAL_CHROME_EXECUTABLE =
+  '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+// import chromium from 'chrome-aws-lambda'
+// import puppeteer from 'puppeteer'
 
 import { DigestHandler } from './digestLink'
-
-// const puppeteer = require('puppeteer-core')
 
 const AMAZON_URLS = [
   'amazon.ca',
@@ -50,14 +67,69 @@ export const fetchAmazonLink: DigestHandler = async (originalLink: string) => {
       originalLink.match(AMAZON_REGEX)?.[0] || originalLink.replace(/\?.*/g, '')
     }?tag=${process.env.AMAZON_ASSOCIATE_ID}`
 
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless:
-        process.env.NODE_ENV === 'development' ? 'new' : chromium.headless,
-      ignoreHTTPSErrors: true,
-      // headless: 'new',
+    // browser = await puppeteer.launch({
+    //   args: chromium.args,
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath: await chromium.executablePath,
+    //   headless:
+    //     process.env.NODE_ENV === 'development' ? 'new' : chromium.headless,
+    //   ignoreHTTPSErrors: true,
+    //   // headless: 'new',
+    // })
+
+    // browser = await puppeteer.launch({
+    //   args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+    //   defaultViewport: chrome.defaultViewport,
+    //   executablePath: await chrome.executablePath,
+    //   headless: true,
+    //   ignoreHTTPSErrors: true,
+    // })
+
+    const executablePath =
+      (await chrome.executablePath) || LOCAL_CHROME_EXECUTABLE
+
+    const browser = await puppeteer.launch({
+      executablePath,
+      // args: chrome.args,
+      args: [
+        ...chrome.args,
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
+      ],
+      headless: process.env.NODE_ENV === 'development' ? 'new' : false,
     })
 
     const page = await browser.newPage()
