305bd9e6d809ff013dfd9142131b00ab83033765
Jake Hash <1508098+jhash@users.noreply.github.com>
2023-08-30 18:58:50 -0400

    Supporting page in url for explore page

--- web/src/pages/ExplorePage/ExplorePage.tsx
+++ web/src/pages/ExplorePage/ExplorePage.tsx
@@ -4,13 +4,13 @@ import ListFadeOut from 'src/components/ListFadeOut/ListFadeOut'
 import PublicListsCell from 'src/components/PublicListsCell'
 import HomeContainerLayout from 'src/layouts/HomeContainerLayout/HomeContainerLayout'
 
-const ExplorePage = () => {
+const ExplorePage = ({ page }) => {
   return (
     <>
       <MetaTags title="Explore" description="Explore page" />
       <HomeContainerLayout>
         <h1 className="font-serif text-5xl leading-tight">{'Explore'}</h1>
-        <PublicListsCell />
+        <PublicListsCell page={page} />
         <ListFadeOut />
       </HomeContainerLayout>
     </>
