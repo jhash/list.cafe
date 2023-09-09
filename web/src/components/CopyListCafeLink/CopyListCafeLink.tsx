92973be01723ffed8fc86fca261acadd04d4f670
Jake Hash <1508098+jhash@users.noreply.github.com>
2023-09-09 00:20:44 -0400

    Wrapped copy to clipboard in browser only and lazy, added where clauses to identifiers query in route hooks

--- web/src/components/CopyListCafeLink/CopyListCafeLink.tsx
+++ web/src/components/CopyListCafeLink/CopyListCafeLink.tsx
@@ -1,20 +1,28 @@
-import { HTMLProps } from 'react'
+import { HTMLProps, Suspense, lazy } from 'react'
 
+import { BrowserOnly } from '@redwoodjs/prerender/browserUtils'
+
+import Loading from 'src/components/Loading'
 import { LIST_CAFE_DOMAIN, LIST_CAFE_URL } from 'src/constants/urls'
 
-import CopyToClipboard from '../CopyToClipboard/CopyToClipboard'
+const CopyToClipboard = lazy(() => import('../CopyToClipboard/CopyToClipboard'))
 
 type CopyListCafeLinkProps = HTMLProps<HTMLAnchorElement> & {
   path: string
 }
 const CopyListCafeLink = ({ path, children }: CopyListCafeLinkProps) => {
   return (
-    <CopyToClipboard
-      text={`${LIST_CAFE_URL}${path[0] === '/' ? '' : '/'}${path}`}
-      message="Copied link to clipboard"
-    >
-      {children || `${LIST_CAFE_DOMAIN}${path[0] === '/' ? '' : '/'}${path}`}
-    </CopyToClipboard>
+    <BrowserOnly>
+      <Suspense fallback={<Loading />}>
+        <CopyToClipboard
+          text={`${LIST_CAFE_URL}${path[0] === '/' ? '' : '/'}${path}`}
+          message="Copied link to clipboard"
+        >
+          {children ||
+            `${LIST_CAFE_DOMAIN}${path[0] === '/' ? '' : '/'}${path}`}
+        </CopyToClipboard>
+      </Suspense>
+    </BrowserOnly>
   )
 }
 
--- web/src/pages/IdentifierPage/IdentifierPage.routeHooks.ts
+++ web/src/pages/IdentifierPage/IdentifierPage.routeHooks.ts
@@ -1,7 +1,50 @@
+import { groupMembershipsWhereClauses } from '../../../../api/src/services/groups/groups'
+import { listMembershipsWhereClauses } from '../../../../api/src/services/lists/lists'
+
 import { db } from '$api/src/lib/db'
 
 export async function routeParameters() {
-  return (await db.identifier.findMany())
+  return (
+    await db.identifier.findMany({
+      where: {
+        OR: [
+          {
+            list: {
+              OR: [
+                { visibility: 'PUBLIC' },
+                { visibility: 'LINK' },
+                ...listMembershipsWhereClauses(),
+              ],
+            },
+          },
+          {
+            person: {
+              OR: [
+                {
+                  user: {
+                    id: context.currentUser?.id,
+                  },
+                },
+                {
+                  visibility: 'PUBLIC',
+                },
+                // TODO: add group check
+              ],
+            },
+          },
+          {
+            group: {
+              OR: [
+                { visibility: 'PUBLIC' },
+                { visibility: 'LINK' },
+                ...groupMembershipsWhereClauses(),
+              ],
+            },
+          },
+        ],
+      },
+    })
+  )
     .filter((identifier) => {
       return (
         !!identifier.listId || !!identifier.groupId || !!identifier.personId
